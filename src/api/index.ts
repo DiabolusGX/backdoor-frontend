c7c91cb4ea5b6d8ccd83d0102da9877549cbb030
Ankit Malik <ankit.malik1092@gmail.com>
2021-07-09 01:45:26 +0530

    deployment changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,36 +4,36 @@ import { ISignUp, ILogIn, IUpdateUser, IPostData, IThreadData, ICommentData } fr
 const API = axios.create();
 
 // Thread API Routes
-export const fetchAllThreads = () => API.get('/threads');
-export const fetchThread = (title: string) => API.get(`/threads/${title}`);
-export const createThread = (threadData: IThreadData) => API.post('/threads/create', threadData);
-export const updateThread = (title: string, threadData: IThreadData) => API.patch('/threads/update', { title, threadData });
+export const fetchAllThreads = () => API.get('/thrd');
+export const fetchThread = (title: string) => API.get(`/thrd/${title}`);
+export const createThread = (threadData: IThreadData) => API.post('/thrd/create', threadData);
+export const updateThread = (title: string, threadData: IThreadData) => API.patch('/thrd/update', { title, threadData });
 
 
 // Posts API Routes
-export const fetchAllPosts = () => API.get('/posts');
-export const fetchPost = (id: string) => API.get(`/posts/${id}`);
-export const fetchPostsBySearch = (title?: string, tags?: string, userId?: string) => API.get(`/posts/search?title=${title}&tags=${tags}&userId=${userId}`);
-export const createPost = (postData: IPostData) => API.post('/posts/create', postData);
-export const updatePost = (id: string, updatedPost: IPostData) => API.patch('/posts/update', { id, updatedPost });
-export const reactPost = (id: string, action: 'like' | 'dislike') => API.patch(`/posts/react?id=${id}&action=${action}`);
-export const deletePost = (id: string) => API.delete(`/posts/delete?id=${id}`);
+export const fetchAllPosts = () => API.get('/psts');
+export const fetchPost = (id: string) => API.get(`/psts/${id}`);
+export const fetchPostsBySearch = (title?: string, tags?: string, userId?: string) => API.get(`/psts/search?title=${title}&tags=${tags}&userId=${userId}`);
+export const createPost = (postData: IPostData) => API.post('/psts/create', postData);
+export const updatePost = (id: string, updatedPost: IPostData) => API.patch('/psts/update', { id, updatedPost });
+export const reactPost = (id: string, action: 'like' | 'dislike') => API.patch(`/psts/react?id=${id}&action=${action}`);
+export const deletePost = (id: string) => API.delete(`/psts/delete?id=${id}`);
 
 
 // Comments API Routes
-export const fetchComment = (commentId: string) => API.get('/comments/' + commentId);
-export const fetchCommentsBySearch = (postId?: string, userId?: string) => API.get(`/comments/search?postId=${postId}&userId=${userId}`);
-export const createComment = (postId: string, comment: ICommentData) => API.post(`/comments/create?postId=${postId}`, comment);
-export const updateComment = (commentId: string, comment: ICommentData) => API.patch(`/comments/update`, { id: commentId, comment });
-export const reactComment = (commentId: string, action: 'like' | 'dislike') => API.patch(`/comments/react?commentId=${commentId}&action=${action}`);
-export const deleteComment = (commentId: string, postId: string) => API.delete(`/comments/delete?postId=${postId}&commentId=${commentId}`);
+export const fetchComment = (commentId: string) => API.get('/cmts/' + commentId);
+export const fetchCommentsBySearch = (postId?: string, userId?: string) => API.get(`/cmts/search?postId=${postId}&userId=${userId}`);
+export const createComment = (postId: string, comment: ICommentData) => API.post(`/cmts/create?postId=${postId}`, comment);
+export const updateComment = (commentId: string, comment: ICommentData) => API.patch(`/cmts/update`, { id: commentId, comment });
+export const reactComment = (commentId: string, action: 'like' | 'dislike') => API.patch(`/cmts/react?commentId=${commentId}&action=${action}`);
+export const deleteComment = (commentId: string, postId: string) => API.delete(`/cmts/delete?postId=${postId}&commentId=${commentId}`);
 
 
 // User API Routes
-export const logout = () => API.post('/user/logout');
-export const login = (loginData: ILogIn) => API.post('/user/login', loginData);
-export const signUp = (signupData: ISignUp) => API.post('/user/signup', signupData);
-export const getUser = (username: string) => API.get(`/user/${username}`);
-export const getUsername = (userId: string) => API.get(`/user/username?userId=${userId}`);
-export const updateUser = (updateData: IUpdateUser) => API.patch('/user/update', updateData);
-export const checkAuthenticated = () => API.post('/user/check');
+export const logout = () => API.post('/usr/logout');
+export const login = (loginData: ILogIn) => API.post('/usr/login', loginData);
+export const signUp = (signupData: ISignUp) => API.post('/usr/signup', signupData);
+export const getUser = (username: string) => API.get(`/usr/${username}`);
+export const getUsername = (userId: string) => API.get(`/usr/username?userId=${userId}`);
+export const updateUser = (updateData: IUpdateUser) => API.patch('/usr/update', updateData);
+export const checkAuthenticated = () => API.post('/usr/check');
--- src/components/Utilities/Sidebar.tsx
+++ src/components/Utilities/Sidebar.tsx
@@ -2,6 +2,8 @@ import { motion } from 'framer-motion';
 import { sidebarVariants } from '../../variants/index';
 import Illustration from '../Utilities/Illustration';
 
+import '../../scss/sidebar.scss';
+
 interface Props {
     logo?: boolean;
     title?: string;
@@ -12,30 +14,51 @@ interface Props {
     bgColor: string;
 }
 
-const Sidebar: React.FC<Props> = props => (
-    <motion.div className={`sticky top-0 left-0 self-start min-h-screen hidden xl:block xl:w-3/12
-    px-5 bg-${props.bgColor} flex flex-col items-start`}
-        variants={sidebarVariants} initial="hidden"
-        animate="visible"
-    >
-        {props.logo &&
-            <h2 className="font-logo text-xl text-red text-left pt-28 mx-2 my-4">Backdoor</h2>
-        }
+const Sidebar: React.FC<Props> = props => {
+    const bgMap = new Map();
+    bgMap.set('yellow', 'sideBgYellow');
+    bgMap.set('grey-lighter', 'sideBgGreyLighter');
 
-        {props.title && (
-            <>
-                <h2 className={`font-display font-medium text-6xl text-${props.titleColor} text-left pt-20 mx-2 my-4`}>
-                    {props.title}
-                </h2>
-                <hr className={`border-2 border-${props.titleColor} rounded-2xl`} />
-            </>
-        )}
+    const titleMap = new Map();
+    titleMap.set('yellow', 'sideTitleYellow');
 
-        <p className={`font-body text-2xl tracking-wider text-${props.descriptionColor} text-left mx-2 mt-4 xl:mb-20 2xl:mb-32`}>
-            {props.description}
-        </p>
-        <Illustration src={props.illustration} />
-    </motion.div>
-);
+    const borderMap = new Map();
+    borderMap.set('yellow', 'sideBorderYellow');
+
+    const descMap = new Map();
+    descMap.set('yellow', 'sideDescYellow');
+    descMap.set('grey-darker', 'sideDescGreyDarker');
+    descMap.set('grey-light', 'sideDescGreyLight');
+
+    return (
+        <motion.div className={`sticky top-0 left-0 self-start min-h-screen hidden xl:block xl:w-3/12
+        px-5 ${bgMap.has(props.bgColor) ? bgMap.get(props.bgColor) : 'sideBgDefault'} flex flex-col items-start`}
+            variants={sidebarVariants} initial="hidden"
+            animate="visible"
+        >
+            {props.logo &&
+                <h2 className="font-logo text-xl text-red text-left pt-28 mx-2 my-4">Backdoor</h2>
+            }
+
+            {props.title && (
+                <>
+                    <h2 className={`font-display font-medium text-6xl `+
+                        `${titleMap.has(props.titleColor) ? titleMap.get(props.titleColor) : 'sideTitleDefault'} `+
+                        `text-left pt-20 mx-2 my-4`}>
+                        {props.title}
+                    </h2>
+                    <hr className={`border-2 ${borderMap.get('yellow')} rounded-2xl`} />
+                </>
+            )}
+
+            <p className={`font-body text-2xl tracking-wider `+
+                `${descMap.has(props.descriptionColor) ? descMap.get(props.descriptionColor) : 'sideDescDefault'} `+
+                `text-left mx-2 mt-4 xl:mb-20 2xl:mb-32`}>
+                {props.description}
+            </p>
+            <Illustration src={props.illustration} />
+        </motion.div>
+    )
+};
 
 export default Sidebar;
